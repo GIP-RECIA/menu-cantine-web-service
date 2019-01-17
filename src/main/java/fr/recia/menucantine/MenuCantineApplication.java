d2d7ab3b7e2a2fea0cb75e6a797590a6adc3a6a2
Pierre Legay <Pierre.Legay@recia.fr>
2019-01-17 15:18:16 +0100

    created SpringBoot with Web Services starter

---
+++ src/main/java/fr/recia/menucantine/MenuCantineApplication.java
@@ -0,0 +1,14 @@
+package fr.recia.menucantine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MenuCantineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MenuCantineApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/fr/recia/menucantine/MenuCantineApplicationTests.java
@@ -0,0 +1,17 @@
+package fr.recia.menucantine;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MenuCantineApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
