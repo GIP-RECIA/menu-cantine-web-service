332881eb3e2dc1f68a4c6132720022f1de40ee91
Pierre Legay <Pierre.Legay@recia.fr>
2019-06-21 11:55:44 +0200

    ServletInitializer sans ErrorPageFilter

--- src/main/java/fr/recia/menucantine/ServletInitializer.java
+++ src/main/java/fr/recia/menucantine/ServletInitializer.java
@@ -5,6 +5,12 @@ import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
 
 public class ServletInitializer extends SpringBootServletInitializer {
 
+	public ServletInitializer(){
+        super();
+        // le bean ErrorPageFilter sera crée dans SecurityConfiguration
+        setRegisterErrorPageFilter(false);
+	}
+
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
 		return application.sources(MenuCantineApplication.class);
