df3ade82c0092d743b3212279d4a6ca5117a2d7c
cailbourdin <nathan.cailbourdin@recia.fr>
2023-12-22 14:14:27 +0100

    refactor: suppression code inutile SecurityConfiguration

--- src/main/java/fr/recia/menucantine/SecurityConfiguration.java
+++ src/main/java/fr/recia/menucantine/SecurityConfiguration.java
@@ -15,7 +15,6 @@
  */
 package fr.recia.menucantine;
 
-
 import java.util.Arrays;
 
 import javax.servlet.Filter;
@@ -32,22 +31,13 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.preauth.AbstractPreAuthenticatedProcessingFilter;
-import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
-import org.springframework.security.web.util.matcher.NegatedRequestMatcher;
-import org.springframework.security.web.util.matcher.RequestMatcher;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-
-
-
 
 @Configuration
 @EnableWebSecurity
@@ -56,47 +46,30 @@ public class SecurityConfiguration{
 	@Value("${soffit.jwt.signatureKey:Changeme}")
     private String signatureKey;
 
-	private static final Logger log = LoggerFactory.getLogger(SecurityConfiguration.class);	
-	@Bean
-    public WebSecurityCustomizer webSecurityCustomizer() throws Exception {
-        /*
-         * Since this module includes portlets, we only want to apply Spring Security to requests
-         * targeting our REST APIs.
-         */
-        final RequestMatcher pathMatcher = new AntPathRequestMatcher("/api/**");
-        final RequestMatcher inverseMatcher = new NegatedRequestMatcher(pathMatcher);
-		log.debug("configure(WebSecurity)");
-        return (web) -> web.ignoring().requestMatchers(inverseMatcher);
-    }
-/*
-	@Override
-	protected void configure(HttpSecurity security) throws Exception
-	{
-		security.httpBasic().disable();
-	}
-*/
+	private static final Logger log = LoggerFactory.getLogger(SecurityConfiguration.class);
 
 	@Bean
     protected SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-		  /*
-         * Provide a SoffitApiPreAuthenticatedProcessingFilter (from uPortal) that is NOT a
-         * top-level bean in the Spring Application Context.
-         */
+		 /*
+		 * Provide a SoffitApiPreAuthenticatedProcessingFilter (from uPortal) that is NOT a
+		 * top-level bean in the Spring Application Context.
+		 */
 		log.debug("configure signatureKey = {}", signatureKey);
         final AbstractPreAuthenticatedProcessingFilter filter =
                 new SoffitApiPreAuthenticatedProcessingFilter(signatureKey);
         
         filter.setAuthenticationManager(authenticationManager());
 
-        http.csrf().disable(); // attention le crsf().disable  est pour les tests a partir de page static (sans csrf)
+        http.csrf().disable(); // attention le crsf().disable est pour les tests a partir de page static (sans csrf)
+
         http
             .addFilter(filter)
             .authorizeRequests()
             	.antMatchers(HttpMethod.GET, "/health-check").anonymous()
                 .antMatchers(HttpMethod.GET,"/api/**").authenticated()
                 .antMatchers(HttpMethod.POST,"/api/menu").authenticated()
-            //   .antMatchers(HttpMethod.DELETE,"/api/**").denyAll()
-             //   .antMatchers(HttpMethod.PUT,"/api/**").denyAll()
+                .antMatchers(HttpMethod.DELETE,"/api/**").denyAll()
+                .antMatchers(HttpMethod.PUT,"/api/**").denyAll()
                 .anyRequest().permitAll() 
             .and() // pour la dev en localhost autorisation du cross domaine
             	.cors()
@@ -118,52 +91,58 @@ public class SecurityConfiguration{
         return new SoffitApiAuthenticationManager();
     }
 
-	 @Bean
-	    public ErrorPageFilter errorPageFilter() {
-	        return new ErrorPageFilter();
-	    }
-	 @Bean
-	    public FilterRegistrationBean<Filter> disableSpringBootErrorFilter() {
-	        /*
-	         * The ErrorPageFilter (Spring) makes extra calls to HttpServletResponse.flushBuffer(),
-	         * and this behavior produces many warnings in the portal logs during portlet requests.
-	         */
-	        FilterRegistrationBean<Filter> filterRegistrationBean = new FilterRegistrationBean<>();
-	        filterRegistrationBean.setFilter(errorPageFilter());
-	        filterRegistrationBean.setEnabled(false);
-	        return filterRegistrationBean;
-	    }
+	@Bean
+	public ErrorPageFilter errorPageFilter() {
+		return new ErrorPageFilter();
+	}
 
-	 @Bean
-	    CorsConfigurationSource corsConfigurationSource() {
-	        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+	@Bean
+	public FilterRegistrationBean<Filter> disableSpringBootErrorFilter() {
+		/*
+		 * The ErrorPageFilter (Spring) makes extra calls to HttpServletResponse.flushBuffer(),
+		 * and this behavior produces many warnings in the portal logs during portlet requests.
+		 */
+		FilterRegistrationBean<Filter> filterRegistrationBean = new FilterRegistrationBean<>();
+		filterRegistrationBean.setFilter(errorPageFilter());
+		filterRegistrationBean.setEnabled(false);
+		return filterRegistrationBean;
+	}
 
-	        boolean abilitaCors = true;
-	        if( abilitaCors )
-	        {
-	            if( log.isWarnEnabled() )
-	            {
-	                log.warn("CORS ABILITATI! CORS est autorisé");
-	            }
-	            CorsConfiguration configuration = new CorsConfiguration();
-	           	            configuration.setAllowedOrigins( 
-	           	            		Arrays.asList("http://localhost:8080",
-	           	            					"https://localhost:8443",
-	           	            					"http://192.168.45.196:8080",
-	           	            					"http://192.168.45.156:8080",
-	           	            					"http://192.168.3.13:8080",
-	           	            					"https://test-lycee.giprecia.net"));
-	            configuration.setAllowedMethods(Arrays.asList(  
-	            		RequestMethod.GET.name(),
-	                    RequestMethod.POST.name(), 
-	                    RequestMethod.OPTIONS.name(), 
-	                    RequestMethod.DELETE.name(),
-	                    RequestMethod.PUT.name()));
-	            
-	            configuration.setExposedHeaders(Arrays.asList("x-auth-token", "x-requested-with", "x-xsrf-token"));
-	            configuration.setAllowedHeaders(Arrays.asList("content-type", "authorization", "x-com-persist", "X-Auth-Token","x-auth-token", "x-requested-with", "x-xsrf-token"));
-	            source.registerCorsConfiguration("/**", configuration);
-	        }
-	        return  source;
-	    }
+	@Bean
+	public CorsConfigurationSource corsConfigurationSource() {
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+
+		boolean abilitaCors = true;
+		if( abilitaCors ) {
+			if( log.isWarnEnabled() ) {
+				log.warn("CORS ABILITATI! CORS est autorisé");
+			}
+			CorsConfiguration configuration = new CorsConfiguration();
+
+			configuration.setAllowedOrigins(Arrays.asList(
+				"http://localhost:8080",
+				"https://localhost:8443",
+				"http://192.168.45.196:8080",
+				"http://192.168.45.156:8080",
+				"http://192.168.3.13:8080",
+				"https://test-lycee.giprecia.net")
+			);
+
+			configuration.setAllowedMethods(Arrays.asList(
+				RequestMethod.GET.name(),
+				RequestMethod.POST.name(),
+				RequestMethod.OPTIONS.name(),
+				RequestMethod.DELETE.name(),
+				RequestMethod.PUT.name())
+			);
+
+			configuration.setExposedHeaders(Arrays.asList("x-auth-token", "x-requested-with", "x-xsrf-token"));
+			configuration.setAllowedHeaders(Arrays.asList("content-type", "authorization", "x-com-persist", "X-Auth-Token","x-auth-token", "x-requested-with", "x-xsrf-token"));
+			source.registerCorsConfiguration("/**", configuration);
+
+			}
+
+		return source;
+
+	}
 }
